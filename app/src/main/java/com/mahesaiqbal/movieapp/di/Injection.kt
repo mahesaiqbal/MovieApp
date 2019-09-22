d36a1e4ddaba49edf4cf52f2f2002f99fb64d50e
mahesaiqbal <iqbalridwansyah.ir@gmail.com>
2019-09-22 23:00:59 +0700

    Add dependency injection

--- app/src/main/java/com/mahesaiqbal/movieapp/di/Injection.kt
+++ app/src/main/java/com/mahesaiqbal/movieapp/di/Injection.kt
@@ -1,2 +1,24 @@
 package com.mahesaiqbal.movieapp.di
 
+import android.app.Application
+import com.mahesaiqbal.movieapp.data.source.MovieRepository
+import com.mahesaiqbal.movieapp.data.source.local.LocalRepository
+import com.mahesaiqbal.movieapp.data.source.local.room.MovieDatabase
+import com.mahesaiqbal.movieapp.data.source.remote.RemoteRepository
+import com.mahesaiqbal.movieapp.utils.AppExecutors
+
+class Injection {
+
+    companion object {
+        fun provideRepository(application: Application): MovieRepository {
+            val database = MovieDatabase.getInstance(application)
+
+            val localRepository = LocalRepository.getInstance(database.movieDao())
+            val remoteRepository = RemoteRepository.getInstance()
+
+            val appExecutors = AppExecutors()
+
+            return MovieRepository.getInstance(localRepository, remoteRepository!!, appExecutors)!!
+        }
+    }
+}
