d9d7ad9425ab37dd8b395cfb78389a854a1a494c
mahesaiqbal <iqbalridwansyah.ir@gmail.com>
2019-09-22 13:05:50 +0700

    Define the database using Room

--- app/src/main/java/com/mahesaiqbal/movieapp/data/source/local/room/MovieDao.kt
+++ app/src/main/java/com/mahesaiqbal/movieapp/data/source/local/room/MovieDao.kt
@@ -1,2 +1,40 @@
 package com.mahesaiqbal.movieapp.data.source.local.room
 
+import androidx.lifecycle.LiveData
+import androidx.paging.DataSource.Factory
+import androidx.room.*
+import com.mahesaiqbal.movieapp.data.source.local.entity.popularmovieentity.PopularMovieEntity
+import com.mahesaiqbal.movieapp.data.source.local.entity.topratedmovieentity.TopRatedMovieEntity
+
+@Dao
+interface MovieDao {
+    @Query("SELECT * FROM popular_movie")
+    fun getPopularMovie(): Factory<Int, PopularMovieEntity>
+
+    @Query("SELECT * FROM popular_movie WHERE favorited = 1")
+    fun getFavoritedPopularMovie(): Factory<Int, PopularMovieEntity>
+
+    @Query("SELECT * FROM top_rated_movie")
+    fun getTopRatedMovie(): Factory<Int, TopRatedMovieEntity>
+
+    @Query("SELECT * FROM top_rated_movie WHERE favorited = 1")
+    fun getFavoritedTopRatedMovie(): Factory<Int, TopRatedMovieEntity>
+
+    @Query("SELECT * FROM popular_movie WHERE id = :movieId")
+    fun getDetailPopularMovie(movieId: Int): LiveData<PopularMovieEntity>
+
+    @Query("SELECT * FROM top_rated_movie WHERE id = :movieId")
+    fun getDetailTopRatedMovie(movieId: Int): LiveData<TopRatedMovieEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertPopularMovie(popularMovie: MutableList<PopularMovieEntity>): LongArray
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertTopRatedMovie(topRatedMovie: MutableList<TopRatedMovieEntity>): LongArray
+
+    @Update(onConflict = OnConflictStrategy.FAIL)
+    fun updatePopularMovie(popularMovie: PopularMovieEntity)
+
+    @Update(onConflict = OnConflictStrategy.FAIL)
+    fun updateTopRatedMovie(topRatedMovie: TopRatedMovieEntity)
+}
--- app/src/main/java/com/mahesaiqbal/movieapp/data/source/local/room/MovieDatabase.kt
+++ app/src/main/java/com/mahesaiqbal/movieapp/data/source/local/room/MovieDatabase.kt
@@ -1,2 +1,39 @@
 package com.mahesaiqbal.movieapp.data.source.local.room
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.mahesaiqbal.movieapp.data.source.local.entity.popularmovieentity.PopularMovieEntity
+import com.mahesaiqbal.movieapp.data.source.local.entity.topratedmovieentity.TopRatedMovieEntity
+import com.mahesaiqbal.movieapp.data.source.local.entity.trailerentity.TrailerMovieEntity
+
+@Database(
+    entities = arrayOf(PopularMovieEntity::class, TopRatedMovieEntity::class, TrailerMovieEntity::class),
+    version = 1,
+    exportSchema = false
+)
+abstract class MovieDatabase : RoomDatabase() {
+
+    abstract fun movieDao(): MovieDao
+
+    companion object {
+
+        private var INSTANCE: MovieDatabase? = null
+
+        private val sLock = Any()
+
+        fun getInstance(context: Context): MovieDatabase {
+            synchronized(sLock) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        MovieDatabase::class.java, "movies.db"
+                    ).build()
+                }
+                return INSTANCE!!
+            }
+        }
+    }
+
+}
