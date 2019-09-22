e5456319c93c77b5682ab715a56695ea5114e589
mahesaiqbal <iqbalridwansyah.ir@gmail.com>
2019-09-22 11:15:57 +0700

    Define Retrofit for networking

--- app/src/main/java/com/mahesaiqbal/movieapp/network/Service.kt
+++ app/src/main/java/com/mahesaiqbal/movieapp/network/Service.kt
@@ -1,2 +1,20 @@
 package com.mahesaiqbal.movieapp.network
 
+import com.mahesaiqbal.movieapp.data.source.remote.response.detail.DetailResponse
+import com.mahesaiqbal.movieapp.data.source.remote.response.popular.PopularMovieResponse
+import com.mahesaiqbal.movieapp.data.source.remote.response.toprated.TopRatedMovieResponse
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface Service {
+    @GET("movie/popular")
+    fun getPopularMovie(@Query("api_key") apiKey: String): Observable<PopularMovieResponse>
+
+    @GET("movie/top_rated")
+    fun getTopRatedMovie(@Query("api_key") apiKey: String): Observable<TopRatedMovieResponse>
+
+    @GET("movie/{movie_id}")
+    fun getDetailMovie(@Path("movie_id") movieId: Int, @Query("api_key") apiKey: String): Observable<DetailResponse>
+}
