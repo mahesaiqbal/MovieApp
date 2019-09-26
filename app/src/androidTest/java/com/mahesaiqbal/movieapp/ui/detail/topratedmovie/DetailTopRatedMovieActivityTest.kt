c017b507c676daf0477603a1934e1f66bf26137b
mahesaiqbal <iqbalridwansyah.ir@gmail.com>
2019-09-26 17:30:39 +0700

    Fix the typo

--- app/src/androidTest/java/com/mahesaiqbal/movieapp/ui/detail/topratedmovie/DetailTopRatedMovieActivityTest.kt
+++ app/src/androidTest/java/com/mahesaiqbal/movieapp/ui/detail/topratedmovie/DetailTopRatedMovieActivityTest.kt
@@ -19,7 +19,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class DetailPopularMovieActivityTest {
+class DetailTopRatedMovieActivityTest {
 
     @Rule
     @JvmField
